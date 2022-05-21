2a20c2222e852081dbf52e275b050c8565610d57
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-05-21 22:57:57 +0930

    refactored redundant code in notes get request

--- routes/notes.js
+++ routes/notes.js
@@ -1,54 +1,43 @@
-const notes = require('express').Router().get('notes');
-const fs = require('fs');
-const { v4: uuidv4 } = require('uuid');
+const notes = require("express").Router().get("notes");
+const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 var notesArray = [];
 
-fs.readFile('./db/db.json', (err, data) => {
-    if(err){
-        console.error(err)
-    }else{
-        notesArray = JSON.parse(data)
+notes.get("/", (req, res) => {
+  fs.readFile("./db/db.json", (err, data) => {
+    if (err) {
+      console.error(err);
     }
-
-})
-
-notes.get('/', (req, res) => {
-    fs.readFile('./db/db.json', (err, data) => {
-        if (err) {
-            console.error(err);
-        }else {
-            res.json(JSON.parse(data));
-        }
-    })
-})
-
-notes.post('/', (req, res) => {
-    const {title, text} = req.body
-
-    if(req.body){
-        const newNote = {
-            title, 
-            text,
-            note_id: uuidv4()
-        }
-        notesArray.push(newNote)
-        
-        fs.writeFile('./db/db.json',JSON.stringify(notesArray),(err) => {
-            if (err) {
-                console.error(err)
-            }else{
-                res.json(notesArray)
-                console.log('Note added successfully')
-            }
-        })
-
-
-
-    }else{
-        res.error('Error in saving note')
+    if (data.length > 0) {
+      notesArray = JSON.parse(data);
     }
+    res.json(notesArray);
+  });
+});
 
-})
+notes.post("/", (req, res) => {
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+    notesArray.push(newNote);
+
+    fs.writeFile("./db/db.json", JSON.stringify(notesArray), (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        res.json(notesArray);
+        console.log("Note added successfully");
+      }
+    });
+  } else {
+    res.error("Error in saving note");
+  }
+});
 
 module.exports = notes;
