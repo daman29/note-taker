b8d726fa4cc87d93ca47be197741b4ce4acb0174
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-05-21 22:26:51 +0930

    added notes route logic

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,12 @@
 const notes = require('express').Router().get('notes');
 const fs = require('fs');
+const { v4: uuidv4 } = require('uuid');
+
+var notesArray = [];
+
+fs.readFile('./db/db.json', (err, data) => {
+    notesArray = JSON.parse(data)
+})
 
 notes.get('/', (req, res) => {
     fs.readFile('./db/db.json', (err, data) => {
@@ -9,6 +16,11 @@ notes.get('/', (req, res) => {
             res.json(JSON.parse(data));
         }
     })
+})
+
+notes.post('/', (req, res) => {
+    notesArray.push(req.body)
+    console.log(notesArray)
 })
 
 module.exports = notes;
