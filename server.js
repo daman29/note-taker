590f865592fbaf0fd5dc701a27ebf14dda89b68c
Damneet Sambhy <damneet.sambhy@hotmail.com>
2022-05-24 15:26:33 +0930

    added wildcard (*) route to redirect all other URL's to homepage

--- server.js
+++ server.js
@@ -20,4 +20,8 @@ app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/notes.html'))
 })
 
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+})
+
 app.listen(PORT, console.log(`Server listening on port ${PORT}`));
